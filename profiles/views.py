6e4c279e1595764af7620a4bbb373428b9cc2f5f
johnson <johnson.iyida@netopng.com>
2020-07-05 17:10:20 +0100

    Added the hello api route/url

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class HelloAPIView(APIView):
+    """Test API VIew"""
+
+    def get(self, request, format=None):
+        """Return the list of APIView features"""
+        an_apiview = [
+            'Uses HTTP method as functions (get, post, pu, delete)',
+            'its similar to a traditional django view',
+            'gives the most control over your application logic',
+            'Is mapped manually to URLS'
+        ]
+
+        return Response({'message': 'Hello!', 'api_view': an_apiview})
