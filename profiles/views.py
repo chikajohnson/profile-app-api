cf9736fcf66d0ba382669c6e2393a2cdd1c1eb51
johnson <johnson.iyida@netopng.com>
2020-07-05 18:12:50 +0100

    Add all http routes

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,13 @@
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from profiles.serializer import HelloSerializer
 
 
 class HelloAPIView(APIView):
     """Test API VIew"""
+    serializer_class = HelloSerializer
 
     def get(self, request, format=None):
         """Return the list of APIView features"""
@@ -15,3 +19,29 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'api_view': an_apiview})
+
+    def post(self, request):
+        """Creates a hello message with our name"""
+
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({"method": 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of object"""
+        return Response({"method": "PATCH"})
+
+    def delete(self, request, pk=None):
+        """Delete a particular object"""
+        return Response({"method": "DELETE"})
+
