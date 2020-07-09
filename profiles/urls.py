f5da7b9b554adb6933dbc49de3825cfcb42412cb
johnson <johnson.iyida@netopng.com>
2020-07-09 06:10:42 +0100

    add login view

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from profiles  import views
+from profiles import views
 
 router = DefaultRouter()
 router.register('viewset', views.HelloViewSet, basename="viewset")
@@ -8,5 +8,6 @@ router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello/', views.HelloAPIView.as_view(), name="hello"),
+    path('login/', views.UserLoginView.as_view()),
     path('', include(router.urls))
 ]
